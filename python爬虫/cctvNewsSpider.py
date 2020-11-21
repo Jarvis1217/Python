2980d5eb16200c5d81526be76113dd85dc2a6c7f
Jarvis1217 <45352073+Jarvis1217@users.noreply.github.com>
2020-11-21 11:36:41 +0800

    Add files via upload

---
+++ python爬虫/cctvNewsSpider.py
@@ -0,0 +1,52 @@
+import time
+from lxml import etree
+from selenium import webdriver
+# 实现无可视化界面
+from selenium.webdriver.chrome.options import Options
+# 实现规避检测
+from selenium.webdriver import ChromeOptions
+
+url = "https://news.cctv.com/"
+
+# 实现无可视化界面的操作
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+
+# 实现规避检测
+option = ChromeOptions()
+option.add_experimental_option('excludeSwitches', ['enable-automation'])
+
+# 如何实现让selenium规避被检测到的风险
+bro = webdriver.Chrome()
+bro.get('https://jingji.cctv.com/')
+x = int(input('请输入需要爬取的页数:'))
+
+i = 1
+while i < x:
+    bro.find_element_by_xpath('/html/body/div[2]/div/div/div/div/div[2]/div[3]/div/div[3]/div/div[3]/i').click()
+    time.sleep(5)
+    i = i + 1
+
+br = webdriver.Chrome()
+
+br.get(url)
+
+# 等待页面加载js代码
+time.sleep(5)
+
+# 模拟点击 "点击加载更多"
+button = br.find_element_by_xpath("//div[@class='ELMTNPMkWvfnaiVjn2XT9PVP190719']/div/div[3]/p")
+button.click()
+
+# 获取渲染后的网页源码
+page = br.page_source
+
+# xpath解析
+html = etree.HTML(page)
+aims = html.xpath("//ul[@id='newslist']/li")
+
+for aim in aims:
+    print(aim.xpath("div[2]/h3/a/text()"))
+
+br.close()
