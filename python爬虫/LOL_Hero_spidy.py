5f587aef8e44a2f6b8b6386eaff2f78cf93e613b
Jarvis1217 <45352073+Jarvis1217@users.noreply.github.com>
2024-12-20 05:57:35 +0800

    Update LOL_Hero_spidy.py

--- python爬虫/LOL_Hero_spidy.py
+++ python爬虫/LOL_Hero_spidy.py
@@ -7,17 +7,9 @@ html = requests.get(url)
 
 js = json.loads(html.content)
 
-hero = []
+heros = []
 for i in range(len(js["hero"])):
-    hero.append("{}-{}".format(js["hero"][i]["name"], js["hero"][i]["title"]))
+    heros.append("{} - {}".format(js["hero"][i]["name"], js["hero"][i]["title"]))
 
-# for i in range(0,140,10):
-#     print(hero[i:i+10])
-# print(hero[-8:])
-
-print("回车键刷新")
-while True:
-    print(random.sample(hero, 3))
-    ch = input("")
-    if ch == "\n":
-        pass
+for i,h in enumerate(heros):
+    print(f"{i+1}.{h}")
