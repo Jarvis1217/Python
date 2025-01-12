b5b1463cda870c211b2a64ad96ff49cc44669aed
Jarvis1217 <45352073+Jarvis1217@users.noreply.github.com>
2025-01-13 04:10:08 +0800

    Add files via upload

---
+++ texas_poker.py
@@ -0,0 +1,44 @@
+import random
+import time
+
+# 生成乱序牌组
+def generate_shuffled_deck():
+    suits = ['♠', '♥', '♣', '♦']
+    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+    deck = [suit + rank for suit in suits for rank in ranks]
+
+    random.shuffle(deck)
+
+    return deck
+
+# 发牌
+def deal_cards(deck, num):
+    if num > len(deck):
+        raise ValueError("没有足够的牌!")
+    
+    dealt_cards = deck[-num:]
+    del deck[-num:]
+    return dealt_cards
+
+deck = generate_shuffled_deck()
+
+computer = deal_cards(deck,2)
+player = deal_cards(deck,2)
+community  = deal_cards(deck,5)
+
+print(f"您的手牌是: {','.join(player)}")
+
+input()
+print(f"翻牌: {','.join(community[:3])}")
+
+input()
+print(f"转牌: {','.join(community[:4])}")
+
+input()
+print(f"河牌: {','.join(community[:5])}")
+
+input()
+print(f"您的手牌是  : {','.join(player)}")
+print(f"电脑的手牌是: {','.join(computer)}")
+print(f"公共牌是    : {','.join(community)}")
