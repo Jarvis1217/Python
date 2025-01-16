bb55343873aa63213b84f74dc778b2e5af8e603f
Jarvis1217 <45352073+Jarvis1217@users.noreply.github.com>
2025-01-17 00:01:11 +0800

    Update texas_poker.py

--- texas_poker.py
+++ texas_poker.py
@@ -6,7 +6,7 @@ def generate_shuffled_deck():
     suits = ['♤', '♡', '♧', '♢']
     ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
-    deck = [ranks + suits for suit in suits for rank in ranks]
+    deck = [rank + suit for suit in suits for rank in ranks]
 
     random.shuffle(deck)
 
