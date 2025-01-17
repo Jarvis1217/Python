35287e19320524311cc23da359296a85ed9629df
Jarvis1217 <45352073+Jarvis1217@users.noreply.github.com>
2025-01-18 00:05:03 +0800

    Update texas_poker.py

--- texas_poker.py
+++ texas_poker.py
@@ -27,14 +27,10 @@ computer = deal_cards(deck,2)
 player = deal_cards(deck,2)
 community  = deal_cards(deck,5)
 
+print(f"公共牌是: {','.join(community)}")
+
+input()
 print(f"电脑手牌是: {','.join(computer)}")
+
+input()
 print(f"您的手牌是: {','.join(player)}")
-
-input()
-print(f"翻牌: {','.join(community[:3])}")
-
-input()
-print(f"转牌: {','.join(community[:4])}")
-
-input()
-print(f"河牌: {','.join(community[:5])}")
